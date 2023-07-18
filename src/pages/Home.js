114af069de46716ce7d3ea8fda516b7134927aea
ferhatergun <eferhat718@gmail.com>
2023-07-18 19:05:38 +0300

    story kısmı yapıldı

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,56 @@
-import { View, Text ,SafeAreaView,StyleSheet,StatusBar} from 'react-native'
+import { View, Text ,SafeAreaView,StyleSheet,StatusBar,Image,FlatList} from 'react-native'
 import React from 'react'
 import Icon from "react-native-vector-icons/Ionicons";
 
 
 export default function Home() {
+
+  const data = [
+    {
+      uri: require('../assets/acun.jpg'),
+      name: 'acunilicali',
+      id: '1'
+    },
+    {
+      uri: require('../assets/demet.jpg'),
+      name: 'demetakalin',
+      id: '2',
+    },
+    {
+      uri: require('../assets/enes.jpg'),
+      name: 'enesbatur',
+      id: '3'
+    },
+    {
+      uri: require('../assets/ufuk.jpg'),
+      name: 'ufuközkan',
+      id: '4'
+    },
+    {
+      uri: require('../assets/acun.jpg'),
+      name: 'acunilicali',
+      id: '5'
+    },
+    {
+      uri: require('../assets/demet.jpg'),
+      name: 'demetakalin',
+      id: '6',
+    },
+    {
+      uri: require('../assets/enes.jpg'),
+      name: 'enesbatur',
+      id: '7'
+    },
+    {
+      uri: require('../assets/ufuk.jpg'),
+      name: 'ufuközkan',
+      id: '8'
+    },
+  ];
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar backgroundColor="white" barStyle="dark-content"  />
+      <StatusBar backgroundColor="white" barStyle="dark-content" />
+      {/* tab bar start */}
       <View style={styles.tabHeader}>
         <View style={styles.headertextDiv}>
           <Text style={styles.headertext}>Instagram</Text>
@@ -15,9 +59,24 @@ export default function Home() {
           <Icon name="heart-outline" size={30}/>
           <Icon name="chatbox-ellipses-outline" size={30}/>
         </View>
+      </View>
+      {/* story start */}
+      <View style={styles.storyDiv}>
+      <FlatList
+      style={{height:'100%'}}
+        data={data}
+        renderItem={({item}) => 
+            <View style={styles.story}>
+              <Image source={item.uri} style={styles.storyImage} />
+              <Text style={styles.storyText}>{item.name}</Text>
+            </View>
+      }
+        keyExtractor={(item) => item.id}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+      />
         
       </View>
-      <View style={styles.storyDiv}></View>
       <View style={styles.contentDiv}></View>
     </SafeAreaView>
   )
@@ -42,8 +101,30 @@ const styles =StyleSheet.create({
     fontSize:20
   },
   storyDiv:{
-    flex:1.5,
-    backgroundColor:'purple'
+    flex:1.8,
+    flexDirection:'row',
+    alignItems:'flex-start',
+    paddingLeft:5
+
+  },
+  story:{
+    width:75,
+    height:75,
+    borderRadius:50,
+    borderWidth:2,
+    borderColor:'pink',
+    padding:2,
+    marginLeft:8
+  },
+  storyImage:{
+    width:'100%',
+    height:'100%',
+    borderRadius:50
+  },
+  storyText:{
+    textAlign:'center',
+    marginTop:5,
+    fontSize:12
   },
   contentDiv:{
     flex:10,
@@ -54,5 +135,6 @@ const styles =StyleSheet.create({
     flexDirection:'row',
     justifyContent:'space-between',
     marginRight:10
-  }
+  },
+
 })
